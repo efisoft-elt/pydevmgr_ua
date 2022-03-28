fcb3dc4733cddad1478fa74d70e0678be347bc19
Sylvain Guieu <sylvain.guieu@gmail.com>
2022-03-28 18:59:08 +0200

    align version for python 3.6 compatibility

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     #data_files=data_files,
     license='CeCILL Free Software License Agreement v2.1',
     long_description=open('README.md').read(),
-    install_requires=[ 'cryptography', 'opcua', 'pydevmgr_core>=0.4.1'], 
+    install_requires=[ 'cryptography', 'opcua', 'pydevmgr_core>=0.4.2'], 
     
     extras_require={
     },
